f13f9591b38bd110291b434243ad10af94106052
ZNL17 <ziyulin17@gmail.com>
2024-04-30 10:44:06 +0200

    IViewTest und ControllerSubTest hinzugefügt

---
+++ SubliminaleWahrnehmungTest/ControllerSubTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubliminaleWahrnehmungTest
+{
+    internal class ControllerSubTest
+    {
+        private IModelSubTest modelSubTest;
+        private IViewSubTest viewSubTest;
+
+        public void controllerSubTest() { }
+
+        public void saveTestergebnis() { }
+
+        public void showZiffern() { }
+        public void showBilder() { }
+
+        public bool checkProband(int idProband, int pwd) { return true; }
+    }
+}
---
+++ SubliminaleWahrnehmungTest/IViewSubTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubliminaleWahrnehmungTest
+{
+    internal interface IViewSubTest
+    {
+        IModelSubTest setModel { get; set; }
+        IControllerSubTest setController { get; set; }
+        void show(string BildPfad);
+        void show(int Ziffer);
+        Testergebnis show();
+    }
+}
