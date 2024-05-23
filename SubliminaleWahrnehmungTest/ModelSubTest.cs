73de783ef7316ee0c64080ab6f83aa61c68b044f
Waste <Waste@MACHTNICHTS>
2024-05-23 08:15:18 +0200

    Klasse "ModelSubTest" hinzugefügt

---
+++ SubliminaleWahrnehmungTest/ModelSubTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubliminaleWahrnehmungTest
+{
+    internal class ModelSubTest
+    {
+        ViewSubTest viewSubTest;
+        IControllerSubTest controllerSubTest;
+
+        ModelSubTest();
+        setView(IViewSubTest View);
+        setController(IControllerSubTest Controller);
+        save(Testergebnis testergebnis);
+
+
+    }
+}
